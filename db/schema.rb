7cc635b0dfe9058602b9b8b1904c13be58daa6e1
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-03-31 16:43:31 +0900

    do rails generate model auction suggested_price:integer user_id:integer post_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_011238) do
+ActiveRecord::Schema.define(version: 2019_03_31_074244) do
+
+  create_table "auctions", force: :cascade do |t|
+    t.integer "suggested_price"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.string "product_name"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
