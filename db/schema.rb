c4a93bcd42fa57782de96ff93f7d470252a698ec
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-03-31 17:12:44 +0900

    [do] rails generate migration posts user_id:integer picture:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_074244) do
+ActiveRecord::Schema.define(version: 2019_03_31_081104) do
 
   create_table "auctions", force: :cascade do |t|
     t.integer "suggested_price"
