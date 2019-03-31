d1f92cc2f27dd66d03ab85a10967a400d1df643f
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-03-31 16:31:41 +0900

    do rails generate model user name:string password:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_011238) do
+ActiveRecord::Schema.define(version: 2019_03_31_073032) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.string "product_name"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
