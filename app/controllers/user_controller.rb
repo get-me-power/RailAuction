752b0f42dabb625d96ae61a9b80dc044133fda70
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-04-01 01:36:04 +0900

    [add] 認証をできるようにした

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,11 +8,18 @@ class UserController < ApplicationController
 
   def login
   end
-
+  #ユーザーの新規登録を行う
   def create
-    @user = User.new(name: params[:name], password: params[:password])
+    @user = User.new(create_params)
     @user.save
     redirect_to("/home/top")
   end
   
+  #User.newするときの引数の関数
+  def create_params
+    params.require(:user).permit(
+      :name,
+      :password
+      )
+  end
 end
