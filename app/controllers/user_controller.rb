ac7a19722dd4260bfc09e61459c0d379a29e0590
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-04-01 01:26:51 +0900

    [add] form_for

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,18 @@
 class UserController < ApplicationController
   def new
-    @user = User.new()
+    @user = User.new
   end
 
   def edit
   end
 
   def login
+  end
+
+  def create
+    @user = User.new(name: params[:name], password: params[:password])
+    @user.save
+    redirect_to("/home/top")
   end
   
 end
