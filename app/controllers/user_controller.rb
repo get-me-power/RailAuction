d73ab0c3d6a2cb3e706b3b4241601160e34847a9
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-04-04 18:33:12 +0900

    [add] ユーザー登録のときのエラー処理を記述した

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,15 +11,20 @@ class UserController < ApplicationController
   #ユーザーの新規登録を行う
   def create
     @user = User.new(create_params)
-    @user.save
-    redirect_to("/home/top")
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/user/#{@user.id}")
+    else
+      render("user/new")
+    end
   end
-  
+
   #User.newするときの引数の関数
   def create_params
     params.require(:user).permit(
       :name,
       :password
-      )
+    )
   end
 end
