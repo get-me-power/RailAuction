2ec07843333b401449db87e65a86c79b6cd370b6
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-04-13 18:00:30 +0900

    if user don't login, can't access posts pages...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_user
-  
+
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
   end
+
+  def authenticate_user
+    if @current_user == nil
+      @error_message = "わかる？　ログインしろって言ってんの！！"
+      redirect_to("/user/login")
+    end
+  end
+
 end
