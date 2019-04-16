34d1b415ed91543a2cd9fb8d683bbe5abc12c3c9
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-04-16 21:06:24 +0900

    add varidates for password minimun

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, {presence: true, length: {maximum: 50}}
   validates :password, {presence: true, length: {maximum: 50}}
+  validates :password, {presence: true, length: {minimum: 7}}
 end
