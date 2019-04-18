eedfc4da41f83efc57d5c083f6bacabd5998af28
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-04-18 15:23:25 +0900

    イ　ン　デ　ン　ト　警　察　で　す

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   get 'user/index'
-	get 'user/edit'
-	get 'user/new'
-	get 'user/login' => 'user#login_form'
+  get 'user/edit'
+  get 'user/new'
+  get 'user/login' => 'user#login_form'
   get 'user/:id' => 'home#top'
   post 'user/create'
   post 'user/logout'
 
-	get 'home/top'
-	get 'home/about'
+  get 'home/top'
+  get 'home/about'
 
-	get 'posts/index'
-	get 'posts/new'
+  get 'posts/index'
+  get 'posts/new'
 
-	#get "posts/index/:post_id/show" => "posts#show"
+  #get "posts/index/:post_id/show" => "posts#show"
   get 'posts/:id' => "posts#show"
-	#get 'posts/index/:post_id/edit' => "posts#edit"
+  #get 'posts/index/:post_id/edit' => "posts#edit"
 
-	post 'posts/create'
-	post 'user/login' => 'user#login'
+  post 'posts/create'
+  post 'user/login' => 'user#login'
 end
