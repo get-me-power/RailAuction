0bc8b62ad71b88c456aa1d1446ad6809e5c95175
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-05-02 12:29:48 +0900

    重複するemailを弾くvalidationを追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, {presence: true, length: {maximum: 50}}
   validates :password, {presence: true, length: {maximum: 50}}
   validates :password, {presence: true, length: {minimum: 7}}
+  validates :email, {uniqueness: true, presence: true}
 end
