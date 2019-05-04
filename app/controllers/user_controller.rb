5cd988e16bff838e2f04879834327c7800327e7b
ruritoblogger <rurito.blog@gmail.com>
2019-05-04 16:05:07 +0900

    fix user images

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,6 +11,7 @@ class UserController < ApplicationController
 
   def login
     @user = User.find_by(login_params)
+    @user.image_name = 'default.jpg'
 
     if @user
       session[:user_id] = @user.id
@@ -39,21 +40,21 @@ class UserController < ApplicationController
 
   def update
     @user = User.find_by(id: params[:id])
-    @user.name = params[:name]
-    @user.email = params[:email]
-    @user.password = params[:password]
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
 
-    if params[:image]
+    if params[:user][:image_name]
       @user.image_name = "#{@user.id}.jpg"
-      image = params[:image]
-      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+      image = params[:user][:image_name]
+      File.binwrite("public/user_images/#{@user.id}", image.read)
     end
 
     if @user.save
       flash[:notice] = "編集しました"
       redirect_to('/posts/index')
     else
-      render('posts/edit')
+      redirect_to("/user/#{@user.id}")
     end
 
   end
