ab3969c30201b679cc3e19f9ff31241c42da329d
ruritoblogger <rurito.blog@gmail.com>
2019-05-17 14:44:37 +0900

    fix post column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_075127) do
+ActiveRecord::Schema.define(version: 2019_05_17_054319) do
 
   create_table "auctions", force: :cascade do |t|
     t.integer "suggested_price"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_05_13_075127) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "isShowed"
-    t.time "endTime"
   end
 
   create_table "chats", force: :cascade do |t|
@@ -43,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_05_13_075127) do
     t.datetime "updated_at", null: false
     t.string "picture"
     t.integer "user_id"
+    t.time "endTime"
   end
 
   create_table "users", force: :cascade do |t|
