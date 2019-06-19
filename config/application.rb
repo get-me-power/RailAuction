7cefb22b040a758ec0b344edaf29b3e8a432ae1f
ruritoblogger <rurito.blog@gmail.com>
2019-06-19 19:21:22 +0900

    add time

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module RailAuction
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # setting time zone
+    config.time_zone = 'Tokyo'
   end
 end
